b525e34b8b40e7ec48fd85ec9e218c8f041765ec
thang551999 <duongphucthangts@gmail.com>
2021-05-18 16:14:24 +0700

    doam

---
+++ src/model/infor/infor.controler.ts
@@ -0,0 +1,42 @@
+import * as express from "express";
+import authMiddleware from "../../middleware/auth.middleware";
+import { getRepository } from "typeorm";
+import PostNotFoundException from "../../exceptions/PostNotFoundException";
+import Controller from "../../interfaces/controller.interface";
+import validationMiddleware from "../../middleware/validation.middleware";
+import Infor from "./infor.entity";
+import RequestWithUser from "../../interfaces/requestWithUser.interface";
+class InforController implements Controller {
+  public path = "/posts";
+  public router = express.Router();
+  private postRepository = getRepository(Infor);
+
+  constructor() {
+    this.initializeRoutes();
+  }
+
+  private initializeRoutes() {
+    this.router.post(this.path, this.createPost);
+    this.router.get(this.path, this.getAllPosts);
+  }
+
+  private createPost = async (
+    request: express.Request,
+    response: express.Response
+  ) => {
+    const postData: Infor = request.body;
+    const newPost = this.postRepository.create(postData);
+    await this.postRepository.save(newPost);
+    response.send(newPost);
+  };
+
+  private getAllPosts = async (
+    request: express.Request,
+    response: express.Response
+  ) => {
+    const posts = await this.postRepository.find();
+    response.send(posts);
+  };
+}
+
+export default InforController;
--- src/server.ts
+++ src/server.ts
@@ -4,7 +4,7 @@ import { createConnection } from "typeorm";
 import App from "./app";
 import config from "./ormconfig";
 import AuthenticationController from "./authentication/authentication.controller";
-
+import InforController from "./model/infor/infor.controler";
 (async () => {
   try {
     await createConnection(config);
@@ -12,6 +12,6 @@ import AuthenticationController from "./authentication/authentication.controller
     console.log("Error while connecting to the database", error);
     return error;
   }
-  const app = new App([new AuthenticationController()]);
+  const app = new App([new AuthenticationController(), new InforController()]);
   app.listen();
 })();
